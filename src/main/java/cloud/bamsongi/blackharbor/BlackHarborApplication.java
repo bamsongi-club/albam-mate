7da13e71d5c2a65fc6b8b9e521b5ffdee48da3aa
JH <jiho4890@gmail.com>
2026-07-20 22:13:26 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/cloud/bamsongi/blackharbor/BlackHarborApplication.java
@@ -0,0 +1,13 @@
+package cloud.bamsongi.blackharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackHarborApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackHarborApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/bamsongi/blackharbor/BlackHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.bamsongi.blackharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackHarborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
