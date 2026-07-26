88c627b74b7d52d97186031fa164eaad26b4efe0
han-yejin <biziyejin8844@naver.com>
2026-07-26 20:50:06 +0900

    feat: 시간 기준 구성 추가

--- src/main/java/cloud/bamsongi/albammate/AlbamMateApplication.java
+++ src/main/java/cloud/bamsongi/albammate/AlbamMateApplication.java
@@ -1,5 +1,6 @@
 package cloud.bamsongi.albammate;
 
+import cloud.bamsongi.albammate.global.time.UtcTimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlbamMateApplication {
 
     public static void main(String[] args) {
+        UtcTimeZone.configure();
         SpringApplication.run(AlbamMateApplication.class, args);
     }
 }
