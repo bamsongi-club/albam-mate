7f0cd2ddbb7317b5e27c7d54a0e009195f549996
Jiwon Yang <g.one9709@gmail.com>
2026-07-27 17:23:47 +0900

    feat: 방 생성 기능 구현

---
+++ src/main/java/cloud/bamsongi/albammate/room/repository/RoomRepository.java
@@ -0,0 +1,6 @@
+package cloud.bamsongi.albammate.room.repository;
+
+import cloud.bamsongi.albammate.room.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {}
