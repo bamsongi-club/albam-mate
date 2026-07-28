a05db0d768eaf9433a5e9aa849e2c48895be2377
silverThunder09 <jeg9017@gmail.com>
2026-07-28 18:45:29 +0900

    fix: 외부 데이터소스 환경변수 테스트 격리

--- src/test/java/cloud/bamsongi/albammate/AlbamMateApplicationTests.java
+++ src/test/java/cloud/bamsongi/albammate/AlbamMateApplicationTests.java
@@ -1,11 +1,26 @@
 package cloud.bamsongi.albammate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlbamMateApplicationTests {
 
+    @Autowired private DataSource dataSource;
+
     @Test
     void contextLoads() {}
+
+    @Test
+    void 외부_데이터소스_환경변수가_있어도_H2로_기동한다() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            assertEquals("H2", connection.getMetaData().getDatabaseProductName());
+        }
+    }
 }
