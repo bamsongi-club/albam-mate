3f442f439669c0cf6cdf23035099958ba9dfbed2
Jiwon Yang <g.one9709@gmail.com>
2026-08-20 21:50:34 +0900

    refactor: 매칭 컨트롤러 중복 검증 애노테이션 제거

--- src/main/java/cloud/bamsongi/albammate/matching/controller/MatchController.java
+++ src/main/java/cloud/bamsongi/albammate/matching/controller/MatchController.java
@@ -3,7 +3,6 @@ package cloud.bamsongi.albammate.matching.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +25,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
-@Validated
 @RestController
 @RequestMapping("/api/matches")
 public class MatchController {
