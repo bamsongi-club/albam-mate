9fa5ce20c5a821503e80113caa8cfaf2f52e8caa
Jiwon <g.one9709@gmail.com>
2026-08-21 14:55:16 +0900

    fix: MATCH PostgreSQL 테스트 스케줄러 간섭 격리
    * test: MATCH PostgreSQL 테스트 스케줄러 격리
    
    * style: MATCH 테스트 어노테이션 포맷 정렬

--- src/postgresTest/java/cloud/bamsongi/albammate/matching/MatchProposalClaimPostgresTest.java
+++ src/postgresTest/java/cloud/bamsongi/albammate/matching/MatchProposalClaimPostgresTest.java
@@ -32,7 +32,8 @@ import cloud.bamsongi.albammate.matching.service.command.MatchProposalCoordinato
 import cloud.bamsongi.albammate.testsupport.SharedPostgresIntegrationSupport;
 
 @Testcontainers
-@SpringBootTest(classes = {AlbamMateApplication.class, MatchProposalClaimPostgresTest.ClockSkewConfiguration.class})
+@SpringBootTest(classes = {AlbamMateApplication.class,
+	MatchProposalClaimPostgresTest.ClockSkewConfiguration.class}, properties = "spring.task.scheduling.enabled=false")
 class MatchProposalClaimPostgresTest extends SharedPostgresIntegrationSupport {
 
 	@Autowired
