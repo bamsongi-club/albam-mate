433d72a65d4cd0529c9bb8ae18c56fe77a957002
han-yejin <biziyejin8844@naver.com>
2026-08-23 19:10:00 +0900

    style: format dummy test

--- src/postgresTest/java/cloud/bamsongi/albammate/game/controller/GameSearchControllerPostgresTest.java
+++ src/postgresTest/java/cloud/bamsongi/albammate/game/controller/GameSearchControllerPostgresTest.java
@@ -3,8 +3,8 @@ package cloud.bamsongi.albammate.game.controller;
 import org.junit.jupiter.api.Test;
 
 class GameSearchControllerPostgresTest {
-    @Test
-    void test() {
-        // trigger postgres CI
-    }
+	@Test
+	void test() {
+		// trigger postgres CI
+	}
 }
